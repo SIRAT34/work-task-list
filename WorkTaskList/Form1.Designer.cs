4d3eeba815c22b82b08ba903721f42a1480beefe
SIRAT34 <srt.krcn@hotmail.com>
2026-01-05 16:44:08 +0300

    Add basic WinForms UI

--- WorkTaskList/Form1.Designer.cs
+++ WorkTaskList/Form1.Designer.cs
@@ -1,7 +1,12 @@
-﻿namespace WorkTaskList
+﻿using WorkTaskList.Services;
+using WorkTaskList.Models;
+
+
+namespace WorkTaskList
 {
     partial class Form1
     {
+        private TaskService _taskService = new();
         /// <summary>
         ///  Required designer variable.
         /// </summary>
@@ -28,12 +33,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txtTask = new TextBox();
+            btnAdd = new Button();
+            lstTasks = new ListBox();
+            btnDelete = new Button();
+            btnToggle = new Button();
+            SuspendLayout();
+            // 
+            // txtTask
+            // 
+            txtTask.BackColor = SystemColors.Window;
+            txtTask.Location = new Point(54, 12);
+            txtTask.Name = "txtTask";
+            txtTask.Size = new Size(701, 23);
+            txtTask.TabIndex = 0;
+            txtTask.Text = "Yeni Görev Gir";
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(680, 41);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(75, 23);
+            btnAdd.TabIndex = 1;
+            btnAdd.Text = "Ekle";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // lstTasks
+            // 
+            lstTasks.FormattingEnabled = true;
+            lstTasks.ItemHeight = 15;
+            lstTasks.Location = new Point(12, 175);
+            lstTasks.Name = "lstTasks";
+            lstTasks.Size = new Size(776, 259);
+            lstTasks.TabIndex = 2;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(680, 70);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(75, 23);
+            btnDelete.TabIndex = 3;
+            btnDelete.Text = "Sil";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnToggle
+            // 
+            btnToggle.Location = new Point(670, 132);
+            btnToggle.Name = "btnToggle";
+            btnToggle.Size = new Size(95, 23);
+            btnToggle.TabIndex = 4;
+            btnToggle.Text = "Tamamlandı";
+            btnToggle.UseVisualStyleBackColor = true;
+            btnToggle.Click += btnToggle_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnToggle);
+            Controls.Add(btnDelete);
+            Controls.Add(lstTasks);
+            Controls.Add(btnAdd);
+            Controls.Add(txtTask);
+            Name = "Form1";
+            Text = "WorkTaskList";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtTask;
+        private Button btnAdd;
+        private ListBox lstTasks;
+        private Button btnDelete;
+        private Button btnToggle;
     }
 }
--- WorkTaskList/Form1.cs
+++ WorkTaskList/Form1.cs
@@ -6,5 +6,48 @@ namespace WorkTaskList
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _taskService.Load();
+            RefreshList();
+        }
+
+        private void RefreshList()
+        {
+            lstTasks.Items.Clear();
+
+            foreach (var task in _taskService.GetAll())
+            {
+                string status = task.IsCompleted ? "[TAMAMLANMIÞ] " : "[] ";
+                lstTasks.Items.Add($"{status}{task.Title}");
+            }
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            _taskService.Add(txtTask.Text);
+            txtTask.Clear();
+            RefreshList();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex < 0) return;
+
+            var task = _taskService.GetAll()[lstTasks.SelectedIndex];
+            _taskService.Delete(task.Id);
+            RefreshList();
+        }
+
+        private void btnToggle_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex < 0) return;
+
+            var task = _taskService.GetAll()[lstTasks.SelectedIndex];
+            _taskService.Toggle(task.Id);
+            RefreshList();
+        }
     }
 }
