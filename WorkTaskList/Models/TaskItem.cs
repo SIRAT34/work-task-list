a3113249f3b5866a5c60327174f4e02ad922ce3c
SIRAT34 <srt.krcn@hotmail.com>
2026-01-05 16:15:18 +0300

    Add task model

---
+++ WorkTaskList/Models/TaskItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTaskList.Models
+{
+    public class TaskItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
